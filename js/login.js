c14d45fbb7dfa3ac3480a85bb1abe90141c4f8cc
Chen-Yu-Xiang <44117726+Chen-Yu-Xiang@users.noreply.github.com>
2019-05-13 02:45:34 +0800

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
 login=function(){
-	 var Account_element = document.getElementById('Account');
+	 /*var Account_element = document.getElementById('Account');
 	 var Account= Account_element.value;
 	 var Password_element = document.getElementById('Password');
 	 var Password = Password_element.value;
@@ -18,52 +18,20 @@ login=function(){
 		if (snapshot.empty) {
 			console.log('No matching documents.');
 			alert("帳密錯誤");
-			location.href = "./index.html";
+			//location.href = "./index.html";
 		} 
 		else{
 			snapshot.forEach(doc => {
 				console.log(doc.id, '=>', doc.data());
 				setCookie('id',doc.data()['user_id'],30);
-				
 			});
-			
-			//location.href = "./index.html";
+			location.href = "./index.html";
 		}
 	})
 	.catch(err => {
 		console.log('Error getting documents', err);
 	});
-    alert(getCookie('id'));
- 
- 
+   // alert(getCookie('id'));*/
+	setCookie('id',25,30);
+	
  }
-function setCookie(cname,cvalue,exdays){
-	var d = new Date();
-	d.setTime(d.getTime()+(exdays*24*60*60*1000));
-	var expires = "expires="+d.toGMTString();
-	alert(cname+"="+cvalue+"; "+expires);
-	document.cookie = cname+"="+cvalue+"; "+expires;
-        //alert(document.cookie);
-}
-function getCookie(cname){
-	var name = cname + "=";
-	var ca = document.cookie.split(';');
-	for(var i=0; i<ca.length; i++) {
-		var c = ca[i].trim();
-		if (c.indexOf(name)==0) { return c.substring(name.length,c.length); }
-	}
-	return "";
-}
-function checkCookie(){
-	var user=getCookie("username");
-	alert(user!="");
-	if (user!=""){
-		alert("欢迎 " + user + " 再次访问");
-	}
-	else {
-		user = prompt("请输入你的名字:","");
-  		if (user!="" && user!=null){
-    		setCookie("username",user,30);
-    	}
-	}
-}
